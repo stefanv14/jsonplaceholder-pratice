313f5467746c11c73337267f6ef7706a7d818ebc
stefanv14 <vasilijevic032@gmail.com>
2020-05-11 20:46:41 +0200

    Updates

---
+++ precache-manifest.f6519d5533bb8215cfdfa2e3f27d93c7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3cdd1414b3eb977d9afed214ed30e758",
+    "url": "/line-web-v1/index.html"
+  },
+  {
+    "revision": "4ed729267e8a807ec7f1",
+    "url": "/line-web-v1/static/css/main.8ab25037.chunk.css"
+  },
+  {
+    "revision": "0569bc533919a157ac47",
+    "url": "/line-web-v1/static/js/2.1d623bb9.chunk.js"
+  },
+  {
+    "revision": "4ed729267e8a807ec7f1",
+    "url": "/line-web-v1/static/js/main.07740cf9.chunk.js"
+  },
+  {
+    "revision": "b88260dfbbf902d3713a",
+    "url": "/line-web-v1/static/js/runtime~main.5b97e1ce.js"
+  }
+]);
